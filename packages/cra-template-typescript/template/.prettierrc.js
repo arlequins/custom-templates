c20fa4a84d6670056f87d18cbc87aba5ff7025c7
wonho an <wonho.an@monstar-lab.com>
2022-04-26 09:30:35 +0900

    feat: 0.0.3

--- packages/cra-template-typescript/template/.prettierrc.js
+++ packages/cra-template-typescript/template/.prettierrc.js
@@ -1,11 +1,3 @@
-module.exports = {
-  trailingComma: "es5",
-  singleQuote: false,
-  printWidth: 200,
-  bracketSpacing: true,
-  bracketSameLine: false,
-  tabWidth: 2,
-  semi: true,
-  endOfLine: "lf",
-  arrowParens: "avoid",
-};
+const prettierrc = require('@arlequins/eslint-config-typescript/prettierrc');
+
+module.exports = prettierrc;
